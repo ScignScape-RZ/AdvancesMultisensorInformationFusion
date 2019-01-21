9ad7bca4c1703f83346682e80a7c4d58907c0e27
ScignScape-RZ <osrworkshops@gmail.com>
2019-01-21 08:56:47 -0500

    update

--- cpp/src/rz/rz-kauvir/rz-graph-run/rz-lisp-graph-core-runner.cpp
+++ cpp/src/rz/rz-kauvir/rz-graph-run/rz-lisp-graph-core-runner.cpp
@@ -1,4 +1,10 @@
 
+//           Copyright Nathaniel Christen 2019.
+//  Distributed under the Boost Software License, Version 1.0.
+//     (See accompanying file LICENSE_1_0.txt or copy at
+//           http://www.boost.org/LICENSE_1_0.txt)
+
+
 #include "rz-lisp-graph-core-runner.h"
 
 #include "rz-lisp-graph-core-runner.templates.h"
--- cpp/src/rz/rz-kauvir/rz-graph-valuer/valuer/rz-lisp-graph-valuer.cpp
+++ cpp/src/rz/rz-kauvir/rz-graph-valuer/valuer/rz-lisp-graph-valuer.cpp
@@ -1,4 +1,10 @@
 
+//           Copyright Nathaniel Christen 2019.
+//  Distributed under the Boost Software License, Version 1.0.
+//     (See accompanying file LICENSE_1_0.txt or copy at
+//           http://www.boost.org/LICENSE_1_0.txt)
+
+
 #include "rz-lisp-graph-valuer.h"
 
 #include "rz-graph-visit/rz-lisp-graph-visitor.h"
